7921ebd7b010264e584fe94fdeb4440b370807fd
Ubuntu <ubuntu@ns31073918.ip-51-75-55.eu>
2021-06-06 00:22:08 +0000

    Added Top Guilds Functions.

--- src/controllers/PlayerDeathsController.js
+++ src/controllers/PlayerDeathsController.js
@@ -1,5 +1,10 @@
 const express = require('express');
-const { player_deaths, players, guild_membership } = require('../models');
+const {
+  player_deaths,
+  players,
+  guild_membership,
+  guilds,
+} = require('../models');
 const { Sequelize } = require('sequelize');
 
 const router = express.Router();
@@ -29,7 +34,7 @@ router.get('/killers', async (req, res) => {
     where: { is_player: 1 },
   });
 
-  const cont = [];
+  const totalKills = [];
   let total = 1;
   for (let i = 0; i < getAllDeathsByPlayer.length; i++) {
     if (
@@ -39,7 +44,7 @@ router.get('/killers', async (req, res) => {
     ) {
       total++;
     } else {
-      cont.push({
+      totalKills.push({
         killed_by: getAllDeathsByPlayer[i].killed_by,
         total_kills: total,
       });
@@ -65,19 +70,68 @@ router.get('/killers', async (req, res) => {
     };
   });
 
-  let processedData = cont.map((item, i) =>
-    Object.assign({}, item, returnNameId[i])
-  );
-
   const verifyPlayerHaveGuild = await guild_membership.findAll({
+    attributes: ['player_id', 'guild_id'],
+    order: [['guild_id', 'ASC']],
+    include: [
+      {
+        model: guilds,
+        attributes: ['name'],
+      },
+    ],
     where: {
       player_id: resultOfPlayerId,
     },
   });
 
+  let concatValues = totalKills.map((item, i) =>
+    Object.assign({}, item, returnNameId[i])
+  );
+
+  let allDatasTogether = [];
+  for (const playersInGuild of verifyPlayerHaveGuild) {
+    for (const playersWithKills of concatValues) {
+      if (playersInGuild.player_id === playersWithKills.id) {
+        allDatasTogether.push({
+          killed_by: playersWithKills.killed_by,
+          total_kills: playersWithKills.total_kills,
+          id: playersWithKills.id,
+          guild: playersInGuild.guild_id,
+          guildName: playersInGuild.guild.name,
+        });
+      }
+    }
+  }
+
+  let currentData;
+  let nextData;
+  let totalGuilds = [];
+  let totalGuildKills = 0;
+  for (let i = 0; i < allDatasTogether.length - 1; i++) {
+    currentData = allDatasTogether[i];
+    nextData = allDatasTogether[i + 1];
+
+    if (currentData.guild === nextData.guild) {
+      totalGuildKills += currentData.total_kills + nextData.total_kills;
+      totalGuilds.push({
+        guild: currentData.guild,
+        totalKills: totalGuildKills,
+        guildName: currentData.guildName,
+      });
+    } else {
+      totalGuilds.push({
+        guild: nextData.guild,
+        totalKills: nextData.total_kills,
+        guildName: nextData.guildName,
+      });
+    }
+  }
+
+  const sortedKills = totalKills.sort((a, b) => b.total_kills - a.total_kills);
+
   return res.jsonOK({
-    totalKills: processedData,
-    haveGuilds: verifyPlayerHaveGuild,
+    TopGuilds: totalGuilds,
+    TopKillers: sortedKills,
   });
 });
 
